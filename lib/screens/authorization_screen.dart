e33041dd01297e32fd13b839360b400ecec364d4
Richar Cangui <richarsantiagocangui96@gmail.com>
2024-08-27 20:15:19 -0500

    refactor: Update import statements for login and authorization screens

--- lib/screens/authorization_screen.dart
+++ lib/screens/authorization_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:instagram/screens/login_screen.dart';
 import 'package:instagram/widgets/button.dart';
+import 'package:instagram/widgets/instagram_logo.dart';
 
 // Scaffold [x]
 // Column [x]
@@ -45,12 +45,7 @@ class AuthorizationScreen extends StatelessWidget {
             //   ),
             // ),
 
-            SvgPicture.asset(
-              "assets/svg/instagram_logo.svg",
-              // Esto sirve para cambiar el color del svg
-              // colorFilter: const ColorFilter.mode(Color.fromARGB(255, 234, 196, 193), BlendMode.srcIn),
-              semanticsLabel: 'Instagram',
-            ),
+            const InstagramLogo(),
 
             /// Widget 2
             /// SizedBox -> solamente crea una caja vacía
